251a6faebcb0804fcb0ef2aa75b2c568d1274c65
Boatts <boatseed159@gmail.com>
2025-04-03 19:26:28 +0700

    config

--- src/components/fierbase.js
+++ src/components/fierbase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDFTvgYdwTSbCstFZVZFFz2ZRV7J4qH-lY",
-    authDomain: "loginmern-1b154.firebaseapp.com",
-    projectId: "loginmern-1b154",
-    storageBucket: "loginmern-1b154.firebasestorage.app",
-    messagingSenderId: "446827407488",
-    appId: "1:446827407488:web:f1fcd78fd79d604d421e65",
-    measurementId: "G-5TB5V05XKL"
-  };
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
 
 firebase.initializeApp(firebaseConfig);
 
