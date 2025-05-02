611eda65199929de12b42f733036c4354f73b427
Boatts <boatseed159@gmail.com>
2025-05-02 18:11:15 +0700

    test

--- src/store/userSlice.js
+++ src/store/userSlice.js
@@ -23,7 +23,6 @@ export const userSlice = createSlice({
   },
 })
 
-// Action creators are generated for each case reducer function
 export const { login, logout, incrementByAmount } = userSlice.actions
 
 export default userSlice.reducer
